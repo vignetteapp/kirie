5b0ac1ab16a27912d7ed43d8dd7090cb26aeb9ba
JcdeA <io@fosshost.org>
2022-03-03 04:18:48 +0000

    feat: add linting

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+      domains: ['avatars.githubusercontent.com','vignetteapp.org','res.cloudinary.com','user-images.githubusercontent.com']
+    },
+}
--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,18 +1,23 @@
 import type { NextRequest } from 'next/server'
 
 export default async function middleware(req: NextRequest) {
-  const imageUrl = req.nextUrl.searchParams.get('url')
-  const quality = req.nextUrl.searchParams.get('q')
-  const width = req.nextUrl.searchParams.get('w')
+  const imageUrl = req.nextUrl.searchParams.get(`url`)
+  const quality = req.nextUrl.searchParams.get(`q`)
+  const width = req.nextUrl.searchParams.get(`w`)
 
+  const url = new URL(
+    `http://${
+      process.env.PRODUCTION ? req.nextUrl.host : `localhost:3000`
+    }/_next/image`,
+  )
 
-  const url = new URL(`https://${process.env.PRODUCTION ? req.nextUrl.host : "vignette-web-vignette.vercel.app"}/_next/image`)
+  url.searchParams.set(`url`, imageUrl)
+  url.searchParams.set(`q`, quality)
+  url.searchParams.set(`w`, width)
 
-  url.searchParams.set('url',imageUrl)
-  url.searchParams.set('q',quality)
-  url.searchParams.set('w',width)
+  const res = await fetch(url.toString(), {
+    headers: { accept: req.headers.get(`accept`) },
+  })
 
-  const res = await fetch(url.toString(),{headers:{accept:req.headers.get('accept')}})
-  
   return res
 }
