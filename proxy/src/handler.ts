cf28cef0a1f263abf91da01db3eaceaac014f042
Jeeho Ahn <io@fosshost.org>
2022-03-19 00:06:41 +0900

    feat: fix next/image stuffs

--- proxy/src/handler.ts
+++ proxy/src/handler.ts
@@ -8,20 +8,28 @@ export async function handleRequest(request: Request): Promise<Response> {
 
   accept?.includes('avif') && supports.push("avif")
   newUrl.searchParams.append('imageSupport', supports.join('-'))
-  newUrl.hostname="kirie.vercel.app"
+
+  if (newUrl.searchParams.get('url')?.startsWith('/_next')) {
+    newUrl.hostname = "vignette.vercel.app"
+    newUrl.pathname = newUrl.searchParams.get('url') as string
+  } else {
+    newUrl.hostname = "kirie.vercel.app"
+    newUrl.pathname = "/"
+  }
+  console.log(newUrl.toString())
 
   const cache = caches.default;
   const newRequest = new Request(newUrl.toString(), request)
-  
+
   const data = await cache.match(newRequest);
-  
+
 
   if (data) {
     return data
-    
-  } else{
+
+  } else {
     const res = await fetch(newRequest)
-    if (res.status == 200){
+    if (res.status == 200) {
       await cache.put(newRequest, res.clone())
     }
     return res
