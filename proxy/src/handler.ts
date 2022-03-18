b69fd23bc5ded4f12ded34c671ed8a76c7853432
JcdeA <io@fosshost.org>
2022-03-18 22:45:18 +0000

    feat: more robust url detection

--- proxy/src/handler.ts
+++ proxy/src/handler.ts
@@ -2,28 +2,26 @@
 export async function handleRequest(request: Request): Promise<Response> {
   const supports: string[] = []
   const accept = request.headers.get(`accept`)
-  const newUrl = new URL(request.url)
+  let newUrl = new URL(request.url)
 
   accept?.includes('webp') && supports.push("webp")
 
   accept?.includes('avif') && supports.push("avif")
   newUrl.searchParams.append('imageSupport', supports.join('-'))
 
-  if (newUrl.searchParams.get('url')?.startsWith('/')) {
+  if (newUrl.pathname.startsWith('/_next/image')) {
     newUrl.hostname = "vignette.vercel.app"
-    newUrl.pathname = newUrl.searchParams.get('url') as string
   } else {
     newUrl.hostname = "kirie.vercel.app"
-    newUrl.pathname = "/"
+    newUrl = new URL(newUrl.href)
+    newUrl.pathname= "/" +newUrl.search
   }
-  console.log(newUrl.toString())
 
   const cache = caches.default;
   const newRequest = new Request(newUrl.toString(), request)
   newRequest.headers.set('accept', request.headers.get('accept') as string)
 
   const data = await cache.match(newRequest);
-
 
   if (data) {
     return data
